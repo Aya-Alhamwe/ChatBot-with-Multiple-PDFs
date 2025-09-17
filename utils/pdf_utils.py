72b100552d2e050cc9828e125ba2171671409765
Aya-Alhamwe <ayahamwe6@gmail.com>
2025-09-18 01:38:13 +0300

    pdf_utils.py

--- utils/pdf_utils.py
+++ utils/pdf_utils.py
@@ -6,11 +6,17 @@ def get_pdf_text(pdf_docs):
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
-            text += page.extract_text()
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text + "\n"
     return text
 
 def get_text_chunks(text, model_name):
+
     if model_name == "Google AI":
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=100)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=500,      
+            chunk_overlap=100     
+        )
     chunks = text_splitter.split_text(text)
     return chunks
