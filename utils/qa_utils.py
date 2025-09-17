c8be13c55edd11949b2a133cb117089947a3d725
Aya-Alhamwe <ayahamwe6@gmail.com>
2025-09-18 01:38:44 +0300

    qa_utils.py

--- utils/qa_utils.py
+++ utils/qa_utils.py
@@ -5,15 +5,24 @@ from langchain.prompts import PromptTemplate
 def get_conversational_chain(model_name, api_key=None):
     if model_name == "Google AI":
         prompt_template = """
-        Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-        provided context just say, "answer is not available in the context", don't provide the wrong answer
+Answer the question as detailed as possible from the provided context.
+If the answer is not in the context, just say "answer is not available in the context".
+Do not provide incorrect answers.
 
-        Context:\n {context}?\n
-        Question: \n{question}\n
+Context:
+{context}
 
-        Answer:
-        """
-        model = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.3, google_api_key=api_key)
+Question:
+{question}
+
+Answer:
+"""
+        model = ChatGoogleGenerativeAI(
+            model="gemini-2.0-flash",
+            temperature=0.3,
+            google_api_key=api_key
+        )
         prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+
         chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
         return chain
